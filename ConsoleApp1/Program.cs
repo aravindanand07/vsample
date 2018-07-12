d03296831709eb1ae6107be1244dd13201b955b7
aravindanand07 <aravindanand07.r@gmail.com>
2018-07-12 16:20:18 +0530

    commit 5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,9 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello VCollab");
-            Console.WriteLine("Hello Bengalore"); 
+            Console.WriteLine("Hello VCollab"); 
         }
     }
 }
